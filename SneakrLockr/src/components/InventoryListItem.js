94db37c371d961f2cf2f049f23bfee7eae0478df
Mike Louviere <mike.louviere@hansoninc.com>
2018-08-28 12:06:04 -0400

    - Adding controls - Adjusting layout

--- SneakrLockr/src/components/InventoryListItem.js
+++ SneakrLockr/src/components/InventoryListItem.js
@@ -1,52 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import EditIcon from '-!react-svg-loader!../assets/images/icons/icon-edit.svg';
+import RemoveIcon from '-!react-svg-loader!../assets/images/icons/icon-remove.svg';
+import AddIcon from '-!react-svg-loader!../assets/images/icons/icon-add.svg';
 
-class InventoryListItem extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+// class InventoryListItem extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 	}
 
-	render() {
-		if ( this.props.upcId ) {
-			return (
-				<div className="inventory-items__item" id={this.props.id}>
-					<h3>{this.props.brand}</h3>
-					<div className="inventory-items__meta">
-						<p>Style: {this.props.style}</p>
-						<p>Size: {this.props.size}</p>
-						<p>UPC: {this.props.upcId}</p>
-						<Link to={`/edit/${this.props.id}`}>Edit</Link>
-					</div>
-				</div>
-			);
-		} else {
-			return (
-				<div className="inventory-items__item" id={this.props.id}>
-					<Link to={`/edit/${this.props.id}`}>+ Add Sneaker</Link>
-				</div>
-			);
-		}
+// 	render() {
+// 		if ( this.props.upcId ) {
+// 			return (
+// 				<div className="inventory-items__item" id={this.props.id}>
+// 					<p className="h5">{this.props.brand}</p>
+// 					<div className="inventory-items__meta">
+// 						<p>Style: {this.props.style ? this.props.style : '--'}</p>
+// 						<p>Size: {this.props.size ? this.props.size : '--'}</p>
+// 						<p>UPC: {this.props.upcId}</p>
+// 					</div>
+
+// 					<div className="inventory-items__controls">
+// 						<Link className="inventory-items__edit" to={`/edit/${this.props.id}`}><EditIcon /></Link>
+// 						<Link className="inventory-items__remove" to={`/edit/${this.props.id}`}><RemoveIcon /></Link>
+// 					</div>
+// 				</div>
+// 			);
+// 		} else {
+// 			return (
+// 				<div className="inventory-items__item" id={this.props.id}>
+// 					<Link to={`/edit/${this.props.id}`}>+ Add Sneaker</Link>
+// 				</div>
+// 			);
+// 		}
 		
+// 	}
+// }
+
+const InventoryListItem = ({ dispatch, id, brand, style, size, upcId }) => {
+	// if item already has upcID, display data, otherwise allow add capabilities
+	if ( upcId ) {
+		return (
+			<div className="inventory-items__item" id={id}>
+				<p className="h5">{brand}</p>
+				<div className="inventory-items__meta">
+					<p>Style: {style ? style : '--'}</p>
+					<p>Size: {size ? size : '--'}</p>
+					<p>UPC: {upcId}</p>
+				</div>
+
+				<div className="inventory-items__controls">
+					<Link className="inventory-items__edit" to={`/edit/${id}`}><EditIcon /></Link>
+					<Link className="inventory-items__remove" to={`/edit/${id}`}><RemoveIcon /></Link>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="inventory-items__item">
+				<div className="inventory-items__adder" id={id}>
+					<Link className="inventory-items__add" to={`/edit/${id}`}><AddIcon /></Link>
+				</div>
+			</div>	
+		);
 	}
 }
-
-// const InventoryListItem = ({ dispatch, id, brand, style, size, upcId }) => (
-// 	<div className="inventory-items__item" id={id}>
-// 		<h3>{brand}</h3>
-// 		{upcId ? `
-// 			<div>
-// 				<p>Style: ${style}</p> }
-// 				<p>Size: ${size}</p>
-// 				<p>UPC: ${upcId}</p>
-// 			</div>
-// 		`:`
-// 		<div>
-// 			<p>Style: ${style}</p> }
-
-// 		</div>
-// 		`
-// 		}
-// 	</div>
-// );
 
 export default InventoryListItem;
